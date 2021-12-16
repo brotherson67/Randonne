4dcca215e22270d918c34704e4b85028051b7dfa
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-16 14:09:08 -0700

    fixed issue I was having with getting the server up and running, issue was wit

--- controllers/apiRoutes/index.js
+++ controllers/apiRoutes/index.js
@@ -2,9 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.use(require('./userRoutes'));
-router.use(require('./workRoutes'));
-router.use(require('./socialRoutes'));
-router.use(require('./gearRoutes'));
-router.use(require('./experienceRoutes'));
+// router.use(require('./workRoutes'));
+// router.use(require('./socialRoutes'));
+// router.use(require('./gearRoutes'));
+// router.use(require('./experienceRoutes'));
 
 module.exports = router;
