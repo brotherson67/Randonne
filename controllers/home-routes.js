80cd12150c6afadf8a72f6d415115b53a6ccd7b2
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-17 17:44:28 -0700

    moving branches

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,16 +1,16 @@
 // const router = require("./apiRoutes/gearRoutes");/
 const router = require('express').Router();
 
-// router.get('/', (req, res) => {
-//     res.render('homepage', {
-//         id: 1,
-//         post_url: 'https://handlebarsjs.com/guide/',
-//         title: 'Handlebars Docs',
-//         created_at: New Date()
-//         user: {
-//             username: 'test_user'
-//         }
-//     });
-// });
+router.get('/', (req, res) => {
+    res.render('homepage', {
+        id: 1,
+        post_url: 'https://handlebarsjs.com/guide/',
+        title: 'Handlebars Docs',
+        created_at: New Date()
+        user: {
+            username: 'test_user'
+        }
+    });
+});
 
 module.exports = router;
