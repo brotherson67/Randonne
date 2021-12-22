0f5a3efd45f9d1ea6a1490a238a0e196322ef13a
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-22 00:35:13 -0700

    added comments to maps

--- routes/apiRoutes/mapbox/index.js
+++ routes/apiRoutes/mapbox/index.js
@@ -4,5 +4,10 @@ mapboxgl.accessToken = 'pk.eyJ1IjoibWl0Y2hicm9kZXYiLCJhIjoiY2t4aDZneTVrMjZoNzJya
 var map = new mapboxgl.Map({
 container: 'PUT THE DIV ID FOR OUR MAP HERE',
 // WE CAN CONTROL THE TYPE OF VIEW USING THIS. 
-style: 'mapbox://styles/mapbox/streets-v11'
+style: 'mapbox://styles/mapbox/streets-v11',
+center: 'USER GEOLOCATION',
+zoom: 11
 });
+
+// I FOUND A WAY TO MAKE A 3D TERRAIN MAP. 
+// WE CAN ALSO ADD VECTOR TILE STYLE MAPS
