12e8878fb8af19d721724c962a8299ca23e32557
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-27 16:28:27 -0700

    added asynchronisity to functions for porfile and modal

--- public/javascript/profile-page.js
+++ public/javascript/profile-page.js
@@ -1,4 +1,3 @@
-const { default: ModelManager } = require("sequelize/dist/lib/model-manager");
 
 var q1 = document.getElementById('#question1');
 var q2 = document.getElementById('#question2');
@@ -8,12 +7,13 @@ var q5 = document.getElementById('#question5');
 
 
 
-document.getElementById("#profile-submit").addEventListener('click', submitButton);
+// var submitBtn = document.getElementById("#profile-submit")
+// submitBtn.addEventListener('click', submitButton);
 
-function submitButton (event) {
+async function submitButton (event) {
     event.preventDefault();
 
-//validate profile name and photo arent empty 
+//validate profile name and photo aren't empty 
     var name = document.getElementById('#name');
     var image = document.getElementById('#photo');
     var valid = true;
@@ -40,23 +40,22 @@ var modal = document.getElementById('#question-modal');
 var btn = document.getElementById('#modal-open');
 var close = document.getElementById('#modal-close');
 
-function openModal() {
+async function openModal() {
     modal.style.display('block');
-}
+    console.log('button click');
+    btn.addEventListener('click', openModal);
 
-btn.addEventListener('click', openModal);
+};
 
 
-function closeModal() {
+async function closeModal() {
     modal.style.display('none');
-}
-
-close.addEventListener('click', closeModal);
+    close.addEventListener('click', closeModal);
+};
 
 
 //close on outside click
-document.addEventListener('click', outsideModal);
-function outsideModal(event) {
+async function outsideModal(event) {
   
     if (event.target.closest(window)){
         return;    
@@ -64,6 +63,7 @@ function outsideModal(event) {
     else if (!event.target.closest(".modal")) {
        modal.style.display = 'none'
     }
+    document.addEventListener('click', outsideModal);
 }
 
 
