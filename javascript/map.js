427124ae5fbb6d0f1cb17eb584cf1ffe90cc237a
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-27 15:19:12 -0700

    saving work on js>maps.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,13 +1,26 @@
 // IMPORT ENV
-
+require('dotenv').config();
 // IMPORT MAP OBJECT
 var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 // here the geolocated/latitude and longitude
 
- 
-// CREATE 2D MAP OBJECT
 mapboxgl.accessToken = MAPBOX_API_TOKEN;
-var map = new mapboxgl.Map({
+
+// get user geolocation
+navigator.geolocation.getCurrentPosition(success, err, {
+    enableHighAccuracy: true
+})
+
+// get coordinates from location
+const success = curruserCoord => {
+    console.log(curruserCoord);
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+}
+
+
+// CREATE 2D MAP OBJECT
+const map2 = new mapboxgl.Map({
 container: 'PUT THE DIV ID FOR OUR MAP HERE',
 // WE CAN CONTROL THE TYPE OF VIEW USING THIS. 
 style: 'mapbox://styles/mapbox/streets-v11',
@@ -16,11 +29,13 @@ zoom: 11
 });
 
 // CREATE 3D MAP OBJECT
-const map = new mapboxgl.Map({
+const map3d = new mapboxgl.Map({
     container: 'map',
     zoom: 13.1,
     center: [-114.34411, 32.6141],
     pitch: 85,
     bearing: 80,
     style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
-    });
+});
+
+modules.export = map;
