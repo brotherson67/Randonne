9744508ad311f5f23886f977c8d78b52143b21eb
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-26 19:06:55 -0700

    tweakes to profile routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,6 +2,10 @@ const router = require('express').Router();
 
 const userRoutes = require('./userRoutes');
 
+const profileRoutes = require('./profileFormRoutes');
+
+router.use('/profile', profileRoutes);
+
 
 router.use('/users', userRoutes);
 
