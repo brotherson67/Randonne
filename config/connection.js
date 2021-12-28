7f2a76915bdc3f01c79090c9e71e259fedca0bb1
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-27 20:04:39 -0700

    updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,16 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 // Connect to database
-const sequelize = new Sequelize('climbing_db', 'root', 'SQLPassword', 
+const sequelize = new Sequelize('climbing_db', 'root', 'bridgetcodes', 
     {
-      // host: '127.0.0.1',
+      host: '127.0.0.1',
       dialect: 'mysql',
-      // port: 3306
+      port: 3306
       // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'SQLPassword',
-      database: 'climbing_db'
+      // user: 'root',
+      // // Your MySQL password
+      // password: 'blueSalamander',
+      // database: 
     },
     console.log('Connected to the climbing database.')
   );
