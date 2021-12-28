8299bfefff72bf7aa2cd169f400c9e196e720406
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-27 17:27:18 -0700

    issue9,22 - added testing html map

--- javascript/map.js
+++ javascript/map.js
@@ -1,41 +1,52 @@
 // IMPORT ENV
-require('dotenv').config();
+// require('dotenv').config();
 // IMPORT MAP OBJECT
-var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
+// var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 // here the geolocated/latitude and longitude
 
-mapboxgl.accessToken = MAPBOX_API_TOKEN;
 
-// get user geolocation
-navigator.geolocation.getCurrentPosition(success, err, {
-    enableHighAccuracy: true
-})
+// // get user geolocation
+// navigator.geolocation.getCurrentPosition(success, err, {
+//     enableHighAccuracy: true
+// })
 
-// get coordinates from location
-const success = curruserCoord => {
-    console.log(curruserCoord);
-    let latitude = position.coords.latitude;
-    let longitude = position.coords.longitude;
-}
+// // get coordinates from location
+// const success = curruserCoord => {
+//     console.log(curruserCoord);
+//     let latitude = position.coords.latitude;
+//     let longitude = position.coords.longitude;
+// }
 
 
-// CREATE 2D MAP OBJECT
-const map2 = new mapboxgl.Map({
-container: 'PUT THE DIV ID FOR OUR MAP HERE',
-// WE CAN CONTROL THE TYPE OF VIEW USING THIS. 
+// mapboxgl.accessToken = "pk.eyJ1IjoibWl0Y2hicm9kZXYiLCJhIjoiY2t4aDZneTVrMjZoNzJya3lodmpsYnQzMSJ9.vYeLQf6nI3N9Zq6Y2ejDIA"
+// // CREATE 2D MAP OBJECT
+// const map2 = new mapboxgl.Map({
+// container: 'map',
+// // WE CAN CONTROL THE TYPE OF VIEW USING THIS. 
+// style: 'mapbox://styles/mapbox/streets-v11',
+// center: [0, 0],
+// zoom: 11
+// });
+
+// CANT GET THE ACCESSTOKEN TO WORK USING .env
+mapboxgl.accessToken = 'pk.eyJ1IjoibWl0Y2hicm9kZXYiLCJhIjoiY2t4aDZneTVrMjZoNzJya3lodmpsYnQzMSJ9.vYeLQf6nI3N9Zq6Y2ejDIA';
+const map2D = new mapboxgl.Map({
+container: 'map',
 style: 'mapbox://styles/mapbox/streets-v11',
-center: 'USER GEOLOCATION',
-zoom: 11
+center: [0, 0],
+zoom: 4
 });
 
-// CREATE 3D MAP OBJECT
-const map3d = new mapboxgl.Map({
-    container: 'map',
-    zoom: 13.1,
-    center: [-114.34411, 32.6141],
-    pitch: 85,
-    bearing: 80,
-    style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
-});
 
-modules.export = map;
+
+// // CREATE 3D MAP OBJECT
+// const map3D = new mapboxgl.Map({
+//     container: 'PUT THE DIV ID FOR OUR MAP HERE',
+//     zoom: 13.1,
+//     center: [-114.34411, 32.6141],
+//     pitch: 85,
+//     bearing: 80,
+//     style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y'
+// });
+
+// modules.export = map;
