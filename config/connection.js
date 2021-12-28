50ddbf3c3f7e8a4a9611d4888463d9407be09651
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-27 20:14:56 -0700

    pushing to develop

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,15 @@
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 // Connect to database
-const sequelize = new Sequelize('climbing_db', 'root', 'SQLPassword', 
+const sequelize = new Sequelize('climbing_db', 'root', 'bridgetcodes', 
     {
-      // host: '127.0.0.1',
+      host: '127.0.0.1',
       dialect: 'mysql',
-      // port: 3306
+      port: 3001,
       // Your MySQL username,
       user: 'root',
       // Your MySQL password
-      password: 'SQLPassword',
+      password: 'blueSalamander',
       database: 'climbing_db'
     },
     console.log('Connected to the climbing database.')
