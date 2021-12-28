20c1c9fb9b2bb0d4f0cd39bb3484d1c56adb1bfe
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-27 20:21:50 -0700

    connection merge

--- config/connection.js
+++ config/connection.js
@@ -5,21 +5,12 @@ const sequelize = new Sequelize('climbing_db', 'root', 'bridgetcodes',
     {
       host: '127.0.0.1',
       dialect: 'mysql',
-<<<<<<< HEAD
-      port: 3001,
-      // Your MySQL username,
-      user: 'root',
-      // Your MySQL password
-      password: 'blueSalamander',
-      database: 'climbing_db'
-=======
       port: 3306
       // Your MySQL username,
       // user: 'root',
       // // Your MySQL password
       // password: 'blueSalamander',
       // database: 
->>>>>>> 06c91583e8cba2ad11a77c6713886817e1e27cd3
     },
     console.log('Connected to the climbing database.')
   );
