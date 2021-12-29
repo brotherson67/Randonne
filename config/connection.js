123e2bf4a35e48ebe045d56c6338129cb2decf28
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-28 21:07:23 -0700

    deleted extra JS folder

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 // Connect to database
-<<<<<<< HEAD
 const sequelize = new Sequelize(
   'climbing_db',
   'root',
@@ -14,20 +13,5 @@ const sequelize = new Sequelize(
   }
 );
 
-=======
-const sequelize = new Sequelize('climbing_db', 'root', 'bridgetcodes', 
-    {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
-      // Your MySQL username,
-      // user: 'root',
-      // // Your MySQL password
-      // password: 'blueSalamander',
-      // database: 
-    },
-    console.log('Connected to the climbing database.')
-  );
->>>>>>> ad38202554fcb35e0fca6e1c6e88d058fa79a749
 
 module.exports = sequelize;
