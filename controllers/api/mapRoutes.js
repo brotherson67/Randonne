b5a3b39e7450e3e04abfb4e896d9eb7563e5bf28
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-29 19:48:01 -0700

    commit before changing branches

--- controllers/api/mapRoutes.js
+++ controllers/api/mapRoutes.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 
 router.get('/', (req, res) => {
     // console.log('./partials/map')
-    res.render('./partials/map');
+    res.render('map');
 });
 
 module.exports = router;
