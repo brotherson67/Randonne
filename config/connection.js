4ae6bb2b4d543b68b4a050e49c0ce6d5239e633e
bridgetvon <88166194+bridgetvon@users.noreply.github.com>
2021-12-29 20:54:54 -0700

    localhost to 127.0.0.1

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,18 @@
-
-const Sequelize = require('sequelize');
 require('dotenv').config();
+const mysql = require('mysql2');
+const Sequelize = require('sequelize');
+
 // Connect to database
-// added env in order to avoid password and pport issues
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, 
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306
-      // Your MySQL username,
-      // user: 'root',
-      // // Your MySQL password
-      // password: 'blueSalamander',
-      // database: 
-    },
-    console.log('Connected to the climbing database.')
-  );
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PW,
+  {
+    host: '127.0.0.1',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
 
 //  ///////////
 module.exports = sequelize;
