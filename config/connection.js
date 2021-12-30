e0b3c52765a230d0df877c61d4aba31acf8c15ad
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-29 19:55:04 -0700

    initial commit on branch

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,16 @@
-// const mysql = require('mysql2');
+const mysql = require('mysql2');
 const Sequelize = require('sequelize');
 // Connect to database
-const sequelize = new Sequelize('climbing_db', 'root', 'bridgetcodes', 
-    {
-      host: '127.0.0.1',
-      dialect: 'mysql',
-      port: 3306
-      // Your MySQL username,
-      // user: 'root',
-      // // Your MySQL password
-      // password: 'blueSalamander',
-      // database: 
-    },
-    console.log('Connected to the climbing database.')
-  );
+const sequelize = new Sequelize(
+  'climbing_db',
+  'root',
+  'SQLPassword97',
+  {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3001
+  }
+);
 
 //  ///////////
 module.exports = sequelize;
