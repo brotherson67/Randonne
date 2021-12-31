f6235a743e6d2b85997e5b1c2a546322754fc073
brotherson67 <mitchell.brotherson@gmail.com>
2021-12-31 00:21:41 -0700

    git issues

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
 const Sequelize = require('sequelize');
-const mysql = require('mysql2')
 require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.PW,
+  process.env.DB_PW,
   {
     host: 'localhost',
     dialect: 'mysql',
-    port: 3001
+    port: 3306
   }
 );
 
