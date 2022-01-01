fe33b2e03a484d3850840473bc1a30586d8e8fe2
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2021-12-31 23:17:06 -0700

    form js file changed

--- public/javascript/form.js
+++ public/javascript/form.js
@@ -8,7 +8,7 @@ var q5 = document.getElementById('#question5');
 async function submitButton (event) {
     event.preventDefault();
 
-//validate profile name and photo aren't empty 
+    //validate profile name and photo aren't empty 
     var name = document.getElementById('#name');
     var image = document.getElementById('#photo');
     var valid = true;
@@ -32,51 +32,6 @@ async function submitButton (event) {
 
 var submitBtn = document.getElementById("profile-submit")
 submitBtn.addEventListener("click", submitButton);
-// //get variables for modal
-// var modal = document.getElementById('#question-modal');
-// var btn = document.getElementById('#modal-open');
-// var close = document.getElementById('#modal-close');
-
-// console.log(modal);
-
-// document.addEventListener("DOMContentLoaded", ()=> {
-
-// async function openModal() {
-//     modal.style.display = "block";
-//     console.log(modal);
-//     console.log('button click');
-
-// };
-
-
-// openModal();
-
-
-
-
-// async function closeModal() {
-//     modal.style.display('none');
-//     close.addEventListener('click', closeModal);
-//     console.log("click")
-// };
-
-// closeModal();
-// });
-
-
-//close on outside click
-// async function outsideModal(event) {
-  
-//     if (event.target.closest(window)){
-//         return;    
-//     }
-//     else if (!event.target.closest(".modal")) {
-//        modal.style.display = 'none'
-//     }
-//     document.addEventListener('click', outsideModal);
-// }
-
-// outsideModal();
 
 
 
